103d3b96e02c097e998266b61f1f13493c9da533
Shounak Das <shounakdas@protonmail.com>
2025-07-23 17:18:37 +0000

    Print constructed command before running

--- easy-rdp.sh
+++ easy-rdp.sh
@@ -4,4 +4,8 @@ read -p "Username: " username
 read -p "Password: " password
 read -p "IP/FQDN: " address
 
+echo
+echo "Running command: xfreerdp /u:$username /p:"$password" /v:$address /dynamic-resolution /scale:140"
+echo
+
 xfreerdp /u:$username /p:"$password" /v:$address /dynamic-resolution /scale:140
